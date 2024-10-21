876cfda2aeef5df7a7cbf75ac1c0bd702459be0f
likyoo <1150249769@qq.com>
2024-10-21 12:53:53 +0800

    fix `no attribute 'get_pretrained_cfgs_for_arch'` error

--- BLIP/models/vit.py
+++ BLIP/models/vit.py
@@ -14,7 +14,6 @@ import torch.nn.functional as F
 from functools import partial
 
 from timm.models.vision_transformer import _cfg, PatchEmbed
-from timm.models.registry import register_model
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.helpers import named_apply, adapt_input_conv
 
